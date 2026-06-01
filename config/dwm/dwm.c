e37fc2439b653eb0d5a99268ec5f88320e438c8c
Valentin Ros Duart <valros20@gmail.com>
2026-06-02 00:58:46 +0200

    Patched restoreafterrestart to dwm

--- config/dwm/dwm.c
+++ config/dwm/dwm.c
@@ -1297,24 +1297,76 @@ propertynotify(XEvent *e)
 }
 
 void
+saveSession(void)
+{
+  FILE *fw = fopen(SESSION_FILE, "w");
+  for (Client *c = selmon->clients; c != NULL; c = c->next) { // get all the clients with their tags and write them to the file
+    fprintf(fw, "%lu %u\n", c->win, c->tags);
+  }
+  fclose(fw);
+}
+
+void
+restoreSession(void)
+{
+  // restore session
+  FILE *fr = fopen(SESSION_FILE, "r");
+  if (!fr)
+    return;
+
+  char *str = malloc(23 * sizeof(char)); // allocate enough space for excepted input from text file
+  while (fscanf(fr, "%[^\n] ", str) != EOF) { // read file till the end
+    long unsigned int winId;
+    unsigned int tagsForWin;
+    int check = sscanf(str, "%lu %u", &winId, &tagsForWin); // get data
+    if (check != 2) // break loop if data wasn't read correctly
+      break;
+
+    for (Client *c = selmon->clients; c ; c = c->next) { // add tags to every window by winId
+      if (c->win == winId) {
+        c->tags = tagsForWin;
+          break;
+      }
+    }
+  }
+
+  for (Client *c = selmon->clients; c ; c = c->next) { // refocus on windows
+    focus(c);
+    restack(c->mon);
+  }
+
+  for (Monitor *m = selmon; m; m = m->next) // rearrange all monitors
+    arrange(m);
+
+  free(str);
+  fclose(fr);
+
+  // delete a file
+  remove(SESSION_FILE);
+}
+
+void
 quit(const Arg *arg)
 {
   if(arg->i) restart = 1;
-	running = 0;
+  running = 0;
+
+  if (restart == 1)
+  saveSession();
 }
 
 Monitor *
 recttomon(int x, int y, int w, int h)
 {
-	Monitor *m, *r = selmon;
-	int a, area = 0;
+  Monitor *m, *r = selmon;
+  int a, area = 0;
 
-	for (m = mons; m; m = m->next)
-		if ((a = INTERSECT(x, y, w, h, m)) > area) {
-			area = a;
-			r = m;
-		}
-	return r;
+  for (m = mons; m; m = m->next)
+    if ((a = INTERSECT(x, y, w, h, m)) > area) {
+      area = a;
+      r = m;
+    }
+  return r;
 }
 
 void
@@ -2250,6 +2302,7 @@ main(int argc, char *argv[])
 		die("pledge");
 #endif /* __OpenBSD__ */
 	scan();
+	restoreSession();
 	run();
   if(restart) execvp(argv[0], argv);
 	cleanup();
