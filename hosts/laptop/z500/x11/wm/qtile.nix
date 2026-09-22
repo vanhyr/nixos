a6ca5d6ad82b582fe322947f65fa6cf6d9f73ad6
Valentin Ros Duart <valros20@gmail.com>
2026-09-22 20:49:28 +0200

    Update flake.lock, fixed qtile and lockscreen activating on dimm

--- hosts/laptop/z500/x11/wm/qtile.nix
+++ hosts/laptop/z500/x11/wm/qtile.nix
@@ -2,33 +2,11 @@
   ...
 }:
 {
-  # Check fails, workaround for now (TODO -> look into it)
-  #nixpkgs.overlays = [
-  #  (final: prev: {
-  #    #qtile = prev.qtile.overrideAttrs (_: {
-  #    #  doCheck = false;
-  #    #});
-  #    pythonPackagesExtensions = prev.pythonPackagesExtensions ++ [
-  #      (python-final: python-prev: {
-  #        qtile-extras = python-prev.qtile-extras.overridePythonAttrs (_: {
-  #          doCheck = false;
-  #        });
-  #        #qtile = python-prev.qtile.overrideAttrs (_: {
-  #        #  doCheck = false;
-  #        #});
-  #      })
-  #    ];
-  #  })
-  #];
-  #environment.systemPackages = with pkgs; [
-  #  python313Packages.mypy
-  #  #mypy
-  #];
+  # https://github.com/NixOS/nixpkgs/issues/559128
+  # https://github.com/NixOS/nixpkgs/pull/559192
   services.xserver.windowManager.qtile = {
-    #enable = true; # not passing tests, disabled for now (TODO -> fix)
-    enable = false;
+    enable = true;
     #package = pkgs.python3.pkgs.qtile; #default
-    #package = pkgs.python313Packages.qtile;
     extraPackages = ps: with ps; [
       qtile-extras
     ];
